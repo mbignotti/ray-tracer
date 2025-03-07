b444886d6dfaa45c5e43d9025104cc1ff686ba6b
mbignotti <marco.bignotti@it.abb.com>
2025-03-07 17:28:31 +0100

    feat: setup sample rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,80 @@
 #include "walnut/application.h"
 
 #include "walnut/image.h"
+#include "walnut/timer.h"
+#include "walnut/random.h"
 
 bool g_ApplicationRunning = true;
 
 class ExampleLayer : public Walnut::Layer
 {
+
 public:
 	virtual void OnUIRender() override
 	{
-		ImGui::Begin("Hello");
-		ImGui::Button("Button");
+		ImGui::Begin("Settings");
+		ImGui::Text("Last render time: %.3fms", m_LastRenderTime);
 		ImGui::End();
 
-		ImGui::ShowDemoWindow();
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+
+		ImGui::Begin("Viewport");
+
+		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
+		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
+
+		if (m_Image)
+			ImGui::Image(m_Image->GetDescriptorSet(), {(float)m_Image->GetWidth(), (float)m_Image->GetHeight()});
+
+		ImGui::End();
+		ImGui::PopStyleVar();
+
+		Render();
+
 	}
+
+	void Render()
+	{
+		Walnut::Timer timer;
+
+		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight())
+		{
+			m_Image = std::make_shared<Walnut::Image>(m_ViewportWidth, m_ViewportHeight, Walnut::ImageFormat::RGBA);
+			delete[] m_ImageData;
+			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
+		}
+
+		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++)
+		{
+			m_ImageData[i] = Walnut::Random::UInt();
+			m_ImageData[i] |= 0xff000000;
+		}
+
+		m_Image->SetData(m_ImageData);
+
+		m_LastRenderTime = timer.ElapsedMillis();
+	}
+
+private:
+	std::shared_ptr<Walnut::Image> m_Image;
+
+	uint32_t *m_ImageData = nullptr;
+
+	uint32_t m_ViewportWidth = 0;
+	uint32_t m_ViewportHeight = 0;
+
+	float m_LastRenderTime = 0.0f;
 };
 
-Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
+Walnut::Application *Walnut::CreateApplication(int argc, char **argv)
 {
 	Walnut::ApplicationSpecification spec;
 	spec.Name = "Walnut Example";
 
-	Walnut::Application* app = new Walnut::Application(spec);
+	Walnut::Application *app = new Walnut::Application(spec);
 	app->PushLayer<ExampleLayer>();
 	app->SetMenubarCallback([app]()
-	{
+							{
 		if (ImGui::BeginMenu("File"))
 		{
 			if (ImGui::MenuItem("Exit"))
@@ -33,8 +82,7 @@ Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 				app->Close();
 			}
 			ImGui::EndMenu();
-		}
-	});
+		} });
 	return app;
 }
 
